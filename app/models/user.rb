ebd6037ea1573030de939419e73e43d3e249ff84
jrupinski <rupinskijakub@gmail.com>
2022-05-25 13:42:52 +0200

    Add User model and it's specs

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ApplicationRecord
+  validates :username, :email, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+  validates :username, :email, :session_token, uniqueness: true
+
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    password_hashed = BCrypt::Password.new(self.password_digest)
+    password_hashed.is_password?(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def self.find_by_credentials(username:, password:)
+    user = User.find_by(username:)
+
+    user&.is_password?(password) ? user : nil
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject(:user) { create(:user) }
+
+  context 'validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_uniqueness_of(:session_token) }
+
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
+
+  it 'creates a password when given a password string' do
+    new_user = build(:user, password: 'password')
+    expect(new_user.password_digest).to_not be_nil
+  end
+
+  describe '#is_password?' do
+    it 'verifies if password is wrong' do
+      expect(user.is_password?('wrong password')).to be false
+    end
+
+    it 'verifies if password is correct' do
+      expect(user.is_password?('password')).to be true
+    end
+  end
+
+  describe '::find_by_credentials' do
+    it 'returns User if credentials are correct' do
+      expect(User.find_by_credentials(username: user.username, password: user.password)).to eq user
+    end
+
+    it 'returns nil if credentials are wrong' do
+      expect(User.find_by_credentials(username: user.username, password: 'wrong password')).to be_nil
+      expect(User.find_by_credentials(username: 'unknown_user', password: 'wrong password')).to be_nil
+    end
+  end
+
+  context 'session management' do
+    describe '::generate_session_token' do
+      it { expect(User.generate_session_token).to_not be_nil }
+    end
+
+    describe '#reset_session_token' do
+      it 'generates a new session token and assigns it to user' do
+        user.valid?
+        old_session_token = user.session_token
+        user.reset_session_token!
+        expect(user.session_token).to_not eq old_session_token
+      end
+    end
+  end
+end
