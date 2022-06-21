8b48dba4211631f5da4223e241851671d50e521a
jrupinski <rupinskijakub@gmail.com>
2022-06-21 16:46:49 +0200

    Add nested comments and its view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -52,7 +52,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id)
+    params.require(:comment).permit(:content, :post_id, :parent_comment_id)
   end
 
   def require_author!
